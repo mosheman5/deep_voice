fbf4af59afbb105f5e80e212760f2091cd33bfec
Noam Bressler <noamzbr@gmail.com>
2019-05-07 22:20:20 +0300

    run disp_file for the wav section documented in tag

--- run_validation.m
+++ run_validation.m
@@ -6,11 +6,20 @@ files_in_dir = dir(tagging_folder);
 
 for file_ind = 3:length(files_in_dir)
     tag_path = fullfile(files_in_dir(file_ind).folder,  files_in_dir(file_ind).name);
-    disp(tag_path)
-    wav_path = fullfile(files_in_dir(file_ind).folder,  files_in_dir(file_ind).name);
-    disp(wav_path)
 
+    file_name = files_in_dir(file_ind).name(1:(end-4));
     
+    open_brace_loc = strfind(file_name, '(');
+    close_brace_loc = strfind(file_name, ')');
+    
+    wav_name = [file_name(1:(open_brace_loc(1)-1)) '.wav'];
+
+    wav_path = fullfile(wav_folder,  wav_name);
+    
+     disp_params= sscanf( file_name(open_brace_loc(1):close_brace_loc(1)),  ...
+        '(%d,%d,%d)');
+          
+    disp_file(wav_path, disp_params(1), disp_params(2), disp_params(3));
     
 end
 % 
