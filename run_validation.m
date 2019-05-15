c823b47b14c0f83b2f36c616ab7302f97b7f557c
Noam Bressler <noamzbr@gmail.com>
2019-05-15 22:48:26 +0300

    plot bb from tag file by MichaelFaran
    	* plot the bb
    	* return cell array with bb list {f1,f2,t1,t2,..}

--- run_validation.m
+++ run_validation.m
@@ -23,11 +23,73 @@ for file_ind = 3:length(files_in_dir)
     
     var = getappdata(gui_handle,'var');
     
-    cc_list = produce_best_CC(var, true);
+    CC = produce_best_CC(var, true);
+    
+    [song_cell, social_cell] = bounding_box(gui_handle, tag_path, true);
     
 end
 
-function cc_list = produce_best_CC(var, do_plot)
+function [song_cell, social_cell] = bounding_box(gui_handle, tag_file_path, do_plot)
+
+string_file_contents=fileread(tag_file_path);
+lines_in_file=splitlines(string_file_contents);
+file_one_line='';
+for ii=1:1:length(lines_in_file)
+    file_one_line=horzcat(file_one_line,' ',lines_in_file{ii});
+end
+string_file_contents=file_one_line(1:(length(string_file_contents)-54)); % 54 is length of
+index=find(string_file_contents=='s');
+index_end=length(index);
+%number of social calls
+song_call_string=string_file_contents;
+social_call_string='';
+for ii=1:1:length(index)
+    s_call_container=string_file_contents(index(ii):end);
+    pivot=find(s_call_container==' ',7);
+    index_end(ii)=pivot(7);
+    num=12;%number of letters to the coordiantes of tagging
+    social_call_string=horzcat(social_call_string,string_file_contents((index(ii)+num):index_end(ii)));
+    song_call_string((index):index_end(ii))=repmat('q',1,index_end(ii)-index+1);
+end
+%now build a matrix where I drop off the q sequence
+song_call_string=song_call_string(find (song_call_string~='q'));
+
+
+song_cell=strsplit(song_call_string);
+social_cell=strsplit(social_call_string);
+m=1;
+ii_last_one=-3;
+if do_plot
+    for ii=1:m:(length(song_cell)-3)
+        
+        if ~isempty(str2num(song_cell{1,ii})) && ~isempty(str2num(song_cell{1,ii+3})) && (ii-ii_last_one)>3
+            rectangle('position',[str2num(song_cell{1,ii+2}) str2num(song_cell{1,ii}) (str2num(song_cell{1,ii+3})-str2num(song_cell{1,ii+2})) (str2num(song_cell{1,ii+1})-str2num(song_cell{1,ii}))],'EdgeColor','k',...
+                'LineWidth',3)   ;
+            m=4;
+            ii_last_one=ii;
+        else
+            m=1;
+
+        end
+    end
+    
+    for ii=1:m:(length(social_cell)-3)
+        
+        if ~isempty(str2num(social_cell{1,ii})) && ~isempty(str2num(social_cell{1,ii+3}))
+            rectangle('position',[str2num(social_cell{1,ii+2}) str2num(social_cell{1,ii}) (str2num(social_cell{1,ii+3})-str2num(social_cell{1,ii+2})) (str2num(social_cell{1,ii+1})-str2num(social_cell{1,ii}))],'EdgeColor','r',...
+                'LineWidth',3)   ;
+            m=4;
+        else
+            m=1;
+
+        end
+    end
+    
+    sprintf ('black-song, red-social call');
+end
+end
+
+function CC = produce_best_CC(var, do_plot)
 % this function runs the CC and selection thresholds (entropy, energy)
 % and returns the CC that passed the threshold
 
@@ -39,7 +101,7 @@ low_cut = 100;
 high_cut = 2000;
 perentile_of_power_thresh = 90;
 min_cc_size = 50;
-upper_ent_thresh = 0.9;
+upper_ent_thresh = 0.3;
 
 stft_mag = abs(var.b_mat);
 spl = mag2db(abs(stft_mag));
@@ -74,14 +136,7 @@ if do_plot
     
     axis tight; axis xy;
     
-    setappdata(gui_figure,'var',var);
+%     setappdata(gui_figure,'var',var);
 end
 
 end
-%
-% cells=strsplit(signal_name,')');
-% daphnastr=strcat(cells{1},')');
-%
-%  eval(daphnastr);
-% char=fileread(signal_name);
-% index=find(char=='s');
